cd73be6d6113f94bc65a9853fd42d1975afbe24a
dev.jam.studio <dev.jam.studio@gmail.com>
2023-06-04 16:32:06 +0200

    footer mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -76,9 +76,9 @@ const Footer = ({ siteTitle }) => {
         <div className="md:hidden flex border-t border-black">
           <div className="aspect-square flex-[2] sm:flex-[1] sm:w-[400px] md:flex-[2] sm:h-1/2  flex items-center justify-center border-l md:w-1/3 md:border-black">
             <svg className="object-cover md:h-full md:w-full" viewBox="0 0 254 254" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="127" cy="127" r="127" stroke="black" strokeWidth="0.5" fill="none" />
-              <line x1="0" y1="127" x2="254" y2="127" stroke="black" strokeWidth="0.5" />
-              <line x1="127" y1="0" x2="127" y2="254" stroke="black" strokeWidth="0.5" />
+              <circle cx="127" cy="127" r="127" stroke="black" strokeWidth="1" fill="none" />
+              <line x1="0" y1="127" x2="254" y2="127" stroke="black" strokeWidth="1" />
+              <line x1="127" y1="0" x2="127" y2="254" stroke="black" strokeWidth="1" />
             </svg>
           </div>
           <div className="social-icons w-auto sm:w-1/3 grid-1 border-black border-l">
